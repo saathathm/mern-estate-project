fa8a73ebe05fd7055eefd71c7742193223bda1f5
mohamed saadhath <saathathm@gmail.com>
2023-12-31 11:10:37 +0530

    create a middleware and error function

--- api/index.js
+++ api/index.js
@@ -11,6 +11,17 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
 app.listen(3000, () => {
     connectDB();
     console.log("Service is running on port 3000");
