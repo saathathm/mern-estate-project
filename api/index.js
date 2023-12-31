cbfb6315b0f23b505e970a2c04dd1eb88472ce63
mohamed saadhath <saathathm@gmail.com>
2023-12-31 10:34:32 +0530

    created sign up api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from '../config/db.js';
-const app = express();
 dotenv.config({path: './config/config.env'});
 import userRouter from './routes/UserRoute.js';
+import authRouter from './routes/AuthRoute.js';
 
-app.use('/api/user', userRouter)
+const app = express();
+app.use(express.json());
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
     connectDB();
-    console.log("Service is runn ing on port 3000");
+    console.log("Service is running on port 3000");
 });
