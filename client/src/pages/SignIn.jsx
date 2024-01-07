68267ddfe9420f5d342a14612899839fe5e46599
mohamed saadhath <saathathm@gmail.com>
2024-01-08 05:18:16 +0530

    Redux and SignIn page Completed

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      dispatch(signInStart());
+      const res = await axios.post("/api/auth/signin", formData);
+      dispatch(signInSuccess(res));
+      navigate('/');
+    } catch (err) {
+      dispatch(signInFailure(err.response.data.message));
+    }
+  };
+
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" placeholder='email' className="border p-3 rounded-lg" id='email' onChange={handleChange} />
+        <input type="password" placeholder='password' className="border p-3 rounded-lg" id='password' onChange={handleChange} />
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">{loading ? "Loading..." : "Sign In"}</button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign up</span>
+        </Link>
+      </div>
+      {
+        error ? <div className="bg-slate-700 text-white p-3 rounded-lg mt-5">{error}</div> : <div></div>
+      }
+    </div>
   )
 }
